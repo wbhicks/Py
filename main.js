d31d2f3969456cf16f76d60c6bf6d996fc156b7b
wbhicks <wbhicks@users.noreply.github.com>
2019-04-16 22:25:41 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,15 +1,15 @@
-function slide() {
+function slide3and4() {
     var num = 0, 
-        style1 = document.getElementById('elem1').style, 
-        style2 = document.getElementById('elem2').style;
+        style3 = document.getElementById('elem-3').style, 
+        style4 = document.getElementById('elem-4').style;
     window.setInterval(function () {
         // increase by num 1, reset to 0 at 4
-        num = (num + 1) % 4;
-        style1.marginLeft = (200 * num) + "px"; 
-        style2.marginLeft = (200 * num) + "px"; 
-    }, 3000); // repeat forever, polling every 3 seconds
+        num = (num + 1) % 6;
+        style3.marginLeft = (170 * num) + "px"; 
+        style4.marginLeft = (140 * num) + "px"; 
+    }, 2500); // repeat forever, polling every 2.5 seconds
 }
 
 window.onload = function start() {
-  slide();
+  slide3and4();
 }
