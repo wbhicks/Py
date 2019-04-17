0b10082980af533f4af74d1b704280cfd4262c3b
wbhicks <wbhicks@users.noreply.github.com>
2019-04-17 00:31:50 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,17 +1,54 @@
-function slide3and4() {
-    var num = 0, 
-        style3 = document.getElementById('elem-3').style, 
-        style4 = document.getElementById('elem-4').style;
-    window.setInterval(function () {
-        // increase by num 1, reset to 0 at 11
-        num = (num + 1) % 11;
-        style3.marginLeft = (170 * num) + "px"; 
-        style4.marginLeft = (130 * num) + "px"; 
-    }, 700); // repeat forever, polling every 0.7 seconds
+// 5 per line in this file:
+var adjectiveArray = [
+    'bad', 'big', 'low', 'old', 'wry',
+    'easy', 'full', 'good', 'late', 'sure',
+    'abstract', 'bounded', 'certain', 'little', 'real',
+    'ahistorical', 'flammable', 'international', 'itinerant', 'resurgent',
+    'hallucinatory', 'performative', 'reduplicative', 'transformative', 'unapproachable'
+];
+// 5 per line in this file:
+var vocationArray = [
+    'accountant', 'baker', 'bartender', 'boilermaker', 'butcher', 
+    'carpenter', 'cashier', 'chef', 'clerk', 'dentist', 
+    'driver', 'farmer', 'fishmonger', 'glazier', 'grocer', 
+    'judge', 'miner', 'pipefitter', 'plumber', 'reporter', 
+    'salvor', 'surveyor', 'tailor', 'taxidermist', 'undertaker'
+];
+var adjectiveArrayLength = adjectiveArray.length;
+var vocationArrayLength = vocationArray.length;
+var shuffledIndicesForAdjectiveArray = new Array(adjectiveArrayLength);
+var shuffledIndicesForVocationArray = new Array(vocationArrayLength);
+for (var i = 0; i < adjectiveArrayLength; i++) {
+    shuffledIndicesForAdjectiveArray[i] = i;
+} // they're not yet shuffled
+for (var i = 0; i < vocationArrayLength; i++) {
+    shuffledIndicesForVocationArray[i] = i;
+} // they're not yet shuffled
+
+function durstenfeldShuffle(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
+    }
 }
 
-function passMeToSetInterval() {
-  var d = new Date();
-  var t = d.toLocaleTimeString();
-  document.getElementById("time-here").innerHTML = t;
+function overall() {
+    var indexForIndexForAdjectives = 0, 
+        indexForIndexForVocations = 0,
+        currentAdjective = '', // will be assigned before being read
+        currentVocation = '';  // will be assigned before being read
+    window.setInterval(function () {
+        if (indexForIndexForAdjectives == 0) {
+            durstenfeldShuffle(shuffledIndicesForAdjectiveArray);
+        }
+        if (indexForIndexForVocations == 0) {
+            durstenfeldShuffle(shuffledIndicesForVocationArray);
+        }
+        currentAdjective = adjectiveArray[shuffledIndicesForAdjectiveArray[indexForIndexForAdjectives]];
+        currentVocation = vocationArray[shuffledIndicesForVocationArray[indexForIndexForVocations]];
+        document.getElementById("adjective-here").innerHTML = currentAdjective;
+        document.getElementById("vocation-here").innerHTML = currentVocation;
+        indexForIndexForAdjectives = (indexForIndexForAdjectives + 1) % adjectiveArrayLength;
+        indexForIndexForVocations = (indexForIndexForVocations + 1) % vocationArrayLength;
+    }, 700); // repeat forever, polling every 0.7 seconds
 }
